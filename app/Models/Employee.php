73b8fb638fa845bd0ad202e28061eaf6f757082c
sng82 <samngreen82@gmail.com>
2023-07-27 12:33:36 +0100

    Added relationships to models

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'address',
+        'postcode',
+        'birth_date',
+        'date_hired',
+        'department_id',
+        'state_id',
+        'city_id',
+        'country_id',
+    ];
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function state()
+    {
+        return $this->belongsTo(State::class);
+    }
+
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
 }
